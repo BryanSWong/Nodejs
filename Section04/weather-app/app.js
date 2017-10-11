27d001e521ca4d582e0c54c6c8696a364087b8b6
Bryan <Bwong727@gmail.com>
2017-10-11 13:06:03 -0400

    Completed video 6 of section 4.

--- Section04/weather-app/app.js
+++ Section04/weather-app/app.js
@@ -1,9 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/' +
-    'json?address=63%2060%20102nd%20Street%20Rego%20Park%20New%20York',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=63%2060%20102nd%20Street%20Rego%20Park%20New%20York',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
