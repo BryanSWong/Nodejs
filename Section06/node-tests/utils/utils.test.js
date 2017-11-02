ee88597f713a8ebae134d6c044155b3baa12acf8
Bryan <Bwong727@gmail.com>
2017-11-02 16:20:33 -0400

    Completed video 5 of section 6.

--- Section06/node-tests/utils/utils.test.js
+++ Section06/node-tests/utils/utils.test.js
@@ -2,16 +2,30 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
+it('should add two numbers.', () => {
     let res = utils.add(33, 11);
 
     expect(res).toBe(44).toBeA('number');
 });
 
-it('should square a number', () => {
+it('should async add two numbers.', (done) => {
+    utils.asyncAdd(3, 4, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    });
+});
+
+it('should square a number.', () => {
     let res = utils.square(5);
 
     expect(res).toBe(25).toBeA('number')
+});
+
+it('should async square a number.', (done) => {
+    utils.asyncSquare(3, (square) => {
+        expect(square).toBe(9).toBeA('number');
+        done();
+    });
 });
 
 it('should verify has first name', () => {
