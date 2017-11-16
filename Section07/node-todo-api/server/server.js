7e1d0f4195af42389a1181331a2db1a8cf3fc8e8
Bryan <Bwong727@gmail.com>
2017-11-16 17:16:58 -0500

    Completed video 13 of section 7, validators, types and defaults.

--- Section07/node-todo-api/server/server.js
+++ Section07/node-todo-api/server/server.js
@@ -5,29 +5,52 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-
 // let newTodo = new Todo({
-//     text: 'Cook dinner'
+//     text: '   complete this video'
+// });
+//
+// newTodo.save().then((doc) => {
+//     console.log('Save todo', doc);
+// }, (err) => {
+//     console.log('Unable to save todo.')
 // });
 
-let newTodo = new Todo({
-    text: 'Finish up watching up to video 15',
-    completed: true,
-    completedAt: 11162017
+let User = mongoose.model('User', {
+    user: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    }
 });
 
-newTodo.save().then((doc) => {
-    console.log('Save todo', doc);
+let newUser = new User({
+    user: 'Newguy',
+    email: 'newguy@service.com'
+});
+
+newUser.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
 }, (err) => {
-    console.log('Unable to save todo.')
+    console.log('unable to add User', err);
 });
