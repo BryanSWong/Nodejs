9831b149ad89a11bf098e846cb5afd6ab0a9db3a
Bryan <Bwong727@gmail.com>
2017-11-21 18:15:48 -0500

    completed video 20 from section 7, added the GET /todos/:id.

--- Section07/node-todo-api/server/server.js
+++ Section07/node-todo-api/server/server.js
@@ -1,9 +1,12 @@
+const {ObjectID} = require('mongodb');
+
 let express = require('express');
 let bodyParser = require('body-parser');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+
 
 var app = express();
 
@@ -27,6 +30,25 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((doc) => {
+        if(!doc){
+            return res.status(404).send();
+        }
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send();
+    })
+
+
 });
 
 app.listen(3000, () => {
