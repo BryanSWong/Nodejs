862fa4c2aa9a57663e1bd43bb46c0c47e805bba7
Bryan <Bwong727@gmail.com>
2017-11-24 22:35:26 -0500

    completed video 25 of section 7, added PATCH path.

--- Section07/node-todo-api/server/server.js
+++ Section07/node-todo-api/server/server.js
@@ -1,14 +1,14 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
 
-var app = express();
+let app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -66,6 +66,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
