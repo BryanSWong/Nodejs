bfb65222a7e7afb58428e23ef58c44bf0f38ba01
Bryan <Bwong727@gmail.com>
2017-12-04 23:42:22 -0500

    completed video 5 of section 8, hashing passwords.

--- Section08/node-todo-api/playground/hashing.js
+++ Section08/node-todo-api/playground/hashing.js
@@ -1,15 +1,34 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 19
-};
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+let password = '123abc';
+let checker = "1";
 
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+
+    });
+});
+
+console.log(checker);
+
+let hashedPassword = '$2a$10$ZL9MCsmWdmwQBsSeNFisMe6IbxcamQvEWL6Jal1bMJNrJ0s95h3QC';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// let data = {
+//     id: 19
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// let decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 
 // let message = 'Im am user number 3';
 // let hash = SHA256(message).toString();
